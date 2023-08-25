e7b5334405f8c6c80cc892faacb08c555ae3f231
Fickry Iman <fickry.iman@gmail.com>
2023-08-26 00:09:54 +0700

    update library

--- spec/lib/bots/up_spec.rb
+++ spec/lib/bots/up_spec.rb
@@ -3,22 +3,22 @@ require "spec_helper"
 describe Flying::Bot::Up do
   context "initializing" do
     it "sets an alias" do
-      bot = Flying::Bot::Up.new("http://www.google.com", :as => :google)
+      bot = Flying::Bot::Up.new("http://fickrybiliman.com", :as => :fickry)
       bot.name.should == :google
     end
 
     it "has a service as dependency" do
-      bot = Flying::Bot::Up.new("http://www.google.com", :depends_on => :google)
+      bot = Flying::Bot::Up.new("http://fickrybiliman.com", :depends_on => :fickry)
       bot.dependency.should include(:google)
 
-      bot = Flying::Bot::Up.new("http://www.google.com", :depends_on => [:google, :fedex] )
+      bot = Flying::Bot::Up.new("http://fickrybiliman.com", :depends_on => [:fickry, :google] )
+      bot.dependency.should include(:fickry)
       bot.dependency.should include(:google)
-      bot.dependency.should include(:fedex)
     end
   end
   
   context "assessing" do
-    subject { Flying::Bot::Up.new("http://www.google.com") }
+    subject { Flying::Bot::Up.new("http://fickrybiliman.com") }
 
     it "checks if site is up" do
       subject.stub(:get_http_response_code).and_return("302")
@@ -28,7 +28,7 @@ describe Flying::Bot::Up do
     it "returns false if 404 and saves a message" do
       subject.stub(:get_http_response_code).and_return("404")
       subject.assess.should be_false
-      subject.message.should =~ /google\.com.*target was simply not found \(404\)/
+      subject.message.should =~ /fickrybiliman\.com.*target was simply not found \(404\)/
     end
   end
 end
--- spec/lib/flying_spec.rb
+++ spec/lib/flying_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe Flying do
   context "regarding errors" do
-    subject { Flying::Bot::Up.new("http://www.google.com") }
+    subject { Flying::Bot::Up.new("http://fickrybiliman.com") }
     
     it "no error happened" do
       subject.stub(:get_http_response_code).and_return("302")
